186843cee7b41d4482e4b2d050f0ccca7bc77246
Ruslan Shakirov <ruslanshakir@gmail.com>
2023-01-07 22:29:57 +0300

    progress

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -19,6 +19,30 @@ module.exports = {
       //   // transition: '150ms $ease-in-out'
       // }
     }),
+    require('postcss-functions')({
+      functions: {
+        linear(v1, v2, w1, w2, unit) {
+          if (!['vh', 'vmin', 'vmax'].includes(unit)) {
+            unit = 'vw'
+          }
+
+          v1 = v1.split(' ').map((v) => parseFloat(v))
+          v2 = v2.split(' ').map((v) => parseFloat(v))
+          ;[w1, w2] = [w1, w2].map((v) => parseFloat(v))
+          const list = []
+
+          for (let i = 0; i < v1.length; i++) {
+            const slope = (v2[i] - v1[i]) / (w2 - w1)
+            const calc = `calc(${(v1[i] - w1 * slope).toFixed(3)}px + ${(
+              100 * slope
+            ).toFixed(3)}${unit})`
+            list.push(`clamp(${v1[i]}px, ${calc}, ${v2[i]}px)`)
+          }
+
+          return list.join(' ')
+        }
+      }
+    }),
     // require('postcss-nested'),
     require('tailwindcss/nesting'),
     require('tailwindcss'),
