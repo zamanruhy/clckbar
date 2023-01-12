3d3b1aab5658ed80f31afdc96ec359e1c55ad4ea
Ruslan Shakirov <ruslanshakir@gmail.com>
2023-01-12 08:09:53 +0300

    nav fixes

--- src/main.js
+++ src/main.js
@@ -28,17 +28,17 @@
     if (open) {
       top = window.scrollY
       Object.assign(document.body.style, {
-        overflow: 'hidden',
-        position: 'fixed',
-        top: `${top * -1}px`
+        overflow: 'hidden'
+        // position: 'fixed',
+        // top: `${top * -1}px`
       })
     } else {
       Object.assign(document.body.style, {
-        overflow: '',
-        position: '',
-        top: ''
+        overflow: ''
+        // position: '',
+        // top: ''
       })
-      window.scrollTo(0, top)
+      // window.scrollTo(0, top)
     }
   })
 })()
